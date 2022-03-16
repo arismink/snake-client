6fce5de0fc5b7f6ae98a02a6ecb1aa97f9f98dfc
krismina la <krismina.la@gmail.com>
2022-03-16 23:25:16 +0000

    passed the returned value of connect into setupInput

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
-const { connect } = require('./client')
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function () {
-  process.stdin.on('keypress', (str, cmd) => {
-    if (cmd.ctrl && cmd.name === 'c') {
-      process.exit(0);
-    }
-    console.log(cmd);
-  })
-};
+const { connect } = require('./client');
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
+setupInput(connect());
